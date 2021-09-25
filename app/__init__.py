ade9e6754907cce4114e4ec4dc5ebc89d8e71817
Ze-Veiga98 <81825844+Ze-Veiga98@users.noreply.github.com>
2021-09-25 23:02:43 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 from app.config import Config
 from app.views.main.routes import main
 from app.views.errors.handlers import errors
-from app.views.statistics.routes import blueprint_statistics
+#from app.views.statistics.routes import blueprint_statistics
 from app.views.about.routes import blueprint_about
 from app.views.charts.routes import blueprint_charts
 from app.views.tables.routes import blueprint_tables
@@ -22,7 +22,7 @@ def create_app(config_class=Config):
 def register_blueprints(app):
     app.register_blueprint(main)
     app.register_blueprint(errors)
-    app.register_blueprint(blueprint_statistics)
+   # app.register_blueprint(blueprint_statistics)
     app.register_blueprint(blueprint_about)
     app.register_blueprint(blueprint_charts)
     app.register_blueprint(blueprint_tables)
