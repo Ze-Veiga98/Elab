252a1efc05c572eb08d1bca7b1ff63f11f419d8b
Ze-Veiga98 <81825844+Ze-Veiga98@users.noreply.github.com>
2021-09-27 16:53:15 +0100

    Update routes.py

--- app/views/tables/routes.py
+++ app/views/tables/routes.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template, Blueprint
 
-from app.extensions import cache
+# Não faz sentido ter cache neste endpoint porque senão cada vez que o user submeter uma nova experiencia os dados iniciam onde a experiencia anterior tinha 
+# terminado.
+#from app.extensions import cache
 
 blueprint_tables = Blueprint("tables", __name__, url_prefix="/tables", static_folder="../static")
 
 @blueprint_tables.route("/")
-@cache.cached(timeout=3600, key_prefix="tables")
+#@cache.cached(timeout=3600, key_prefix="tables")
 def tables():
     return render_template('tables/tables.html')
