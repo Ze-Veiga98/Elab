7cb1f73ffa73f3eb9405607863f27f603fbc5c03
Ze-Veiga98 <81825844+Ze-Veiga98@users.noreply.github.com>
2021-09-27 01:49:53 +0100

    Update elab-control.js

--- app/static/base/js/elab-control.js
+++ app/static/base/js/elab-control.js
@@ -1,3 +1,5 @@
+// javascript 
+
 var base_url = "http://127.0.0.1:5000";
 let Results=0;
 var DeltaX=0;
@@ -38,9 +40,11 @@ function start_Pendulum() {
   });
   // não quero fazer pontos no botão start!
   getData(); 
-  makeTable(response);
+  
 
 }
+
+var mytable = [];
 
 function getData(){
   var endpoint =  base_url + '/resultpoint';
@@ -52,6 +56,7 @@ function getData(){
       if (frist == 0)
 		{
 			res = Object.keys(response.Data);
+      
 			buildPlot1(res);
       //buildPlot2(res);  // grafico de temperatura não 
       buildPlot3(res);
@@ -64,19 +69,134 @@ function getData(){
           myStopFunction();
       }
       else{
+       
+       
         if (typeof response.Data === 'object'){
+          
           Plotly.extendTraces('myplot', {x: [[response.Data.Sample_number]],y: [[response.Data.velocity]]}, [0]);
           //Plotly.extendTraces('myplot1', {x: [[response.Data.e_period]],y: [[response.Data.period]]}, [0]);
           Plotly.extendTraces('myplot2', {x: [[response.Data.e_velocity]],y: [[response.Data.velocity]]}, [0]);
-          $('body').append(buildtable(response.data));
-
+          //console.log('ola:'+ response.Data.Sample_number);
+          //console.log('ola:'+ response.Data.);
+          mytable.push(response.Data);
         
+	// create a table
+          var html = "<table>";
+          mytable.forEach(function(entry) {
+          
+          // table body
+          for (var k in entry){
+
+                html += "<td>" + entry[k]  +  "</td>";
+          }
+          html += "</tr >";
+          });
+          html += "</table>";
+          document.getElementById("result").innerHTML = html;
+          
         }
+       
         getData();
       }
+     
     }
   });
 }
+
+function generate_table() {
+  // get the reference for the body
+  var body = document.getElementsByTagName("body")[0];
+
+  // creates a <table> element and a <tbody> element
+  var tbl = document.createElement("table");
+  //var header = document.createElement("header");
+  var header = '<tr><th>fg</th><th>City</th></tr>';
+
+  //var header = "<th>Header</th>";
+  var tblBody = document.createElement("tbody");
+
+
+  // creating all cells
+  for (var i = 0; i < results.weak_sent.length; i++) {
+      // creates a table row
+      var row = document.createElement("tr");
+
+      for (var j = 0; j < 2; j++) {
+          // Create a <td> element and a text node, make the text
+          // node the contents of the <td>, and put the <td> at
+          // the end of the table row
+          var cell = document.createElement("td");
+          if (j == 0) {
+              var cellText = document.createTextNode(results.weak_sent_num[i]);
+          } else {
+              var cellText = document.createTextNode(results.weak_sent[i]);
+          }
+
+
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+      }
+
+      // add the row to the end of the table body
+      tblBody.appendChild(row);
+  }
+  // This is for the quick solution
+  tbl.innerHTML = header
+  // put the <tbody> in the <table>
+  tbl.appendChild(tblBody);
+
+
+
+  // appends <table> into <body>
+  body.appendChild(tbl);
+  // sets the border attribute of tbl to 2;
+  tbl.setAttribute("border", "2");
+}
+
+
+
+
+
+function tablebind() {  
+  $.ajax({  
+      type: "GET",  
+      contentType: "application/json; charset=utf-8",  
+      url: base_url + '/resultpoint',  
+      data: "{}",  
+      contentType: 'application/json;charset=UTF-8',
+      success: function (response) {  
+          var obj = $.parseJSON(response.d);  
+          if (obj.length > 0) {  
+
+              var data = obj[0].Table1;  
+              var table = $("<table />");  
+              table[0].border = "1";  
+
+              var row$;  
+
+              var columns = addAllColumnHeaders(data);  
+              for (var i = 0; i < data.length; i++) {  
+                  row$ = $('<tr/>');  
+           
+                  for (var colIndex = 0; colIndex < columns.length; colIndex++) {  
+                      var cellValue = data[i][columns[colIndex]];  
+
+                      if (cellValue == null) { cellValue = ""; }  
+
+                      row$.append($('<td/>').html(cellValue));  
+                  }  
+                  $("#jsonTable").append(row$);  
+              }  
+               
+          }  
+
+      },  
+      error: function (response) {  
+          //                      
+      }  
+  });  
+
+}  
 
 
 
@@ -243,21 +363,7 @@ function buildPlot3(res) {
 
      Plotly.newPlot('myplot2', output_data, layout);
    
-}
-
-
-function buildtable(tableData) {
-  var table = $('<table></table>');
-  $(tableData).each(function (i, rowData) {
-      var row = $('<tr></tr>');
-      $(rowData).each(function (j, cellData) {
-          row.append($('<td>'+cellData+'</td>'));
-      });
-      table.append(row);
-  });
-  return table;
-}
-
+    }
 
 
 
